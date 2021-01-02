f5865f35a179539c394f69b5a08928eda68974bf
Luis Salas <luisszkl@gmail.com>
2021-01-02 14:44:16 +0000

    Updates region endpoint

--- api/src/controllers/regiones.js
+++ api/src/controllers/regiones.js
@@ -3,8 +3,31 @@ const db = require("../models");
 
 // eslint-disable-next-line no-unused-vars
 exports.getAllRegiones = async (req, res, next) => {
-  const allRegiones = await db.Region.findAll({
-    attributes: ["id", "region", "codigo", "quotas", "curul"]
+  let allRegiones = await db.Region.findAll({
+    attributes: ["id", "region", "codigo", "quotas", "curul", "idOrgPol"]
   });
+  const regex = /c\(/;
+  const regex2 = /\)/;
+  const regex3 = /([0-9]{0,})/gi;
+
+  function parse(item) {
+    if (item.dataValues.idOrgPol) {
+      let matchesArray = [];
+      const matches = item.dataValues.idOrgPol
+        .replace(regex, "{")
+        .replace(regex2, "}")
+        .match(regex3);
+      matches.forEach(element => {
+        if (element) {
+          matchesArray.push(element);
+        }
+      });
+      item.dataValues.idOrgPol = matchesArray;
+    }
+
+    return item;
+  }
+  allRegiones = allRegiones.map(parse);
+
   res.status(200).send(allRegiones);
 };
