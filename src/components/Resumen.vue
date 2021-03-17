a12eba41dbcc83782a871468bf59f1136a207a9e
Antonio Cucho <antoniocuga@gmail.com>
2021-03-17 13:10:43 -0400

    Update resumen

--- src/components/Resumen.vue
+++ src/components/Resumen.vue
@@ -1,280 +1,135 @@
 <template>
-  <div>
-    <v-row>
-      <v-flex md6>
-        <v-select
-          :items="tablasResumen"
-          item-text="value"
-          item-value="id"
-          label="Elige un Resúmen:"
-          v-model="currentResumen"
-          prepend-icon="mdi-table"
-          color="secondary"
-          :return-object="true"
-          v-on:change="updateTabla"
-        ></v-select>
-      </v-flex>
-    </v-row>
-    <v-row>
-      <v-flex>
-        <line-chart
-          v-if="datacollection"
-          :chart-data="datacollection"
-          :options="options"
-        ></line-chart>
-      </v-flex>
-      <v-flex md4>
-        <v-data-table
-          v-if="datacollection"
-          :headers="getHeaders()"
-          :items="currentTabla"
-        >
-        </v-data-table>
-      </v-flex>
-    </v-row>
+  <div class="partido-ranking-container mt-5 pt-5">
+    <h2 class="text-center">
+      Compara a los partidos que participan en las Elecciones 2021
+    </h2>
+    <h3 class="text-center">
+      Ordenalos por tus intereses
+    </h3>
+    <div class="row mb-5">
+      <v-col
+      cols="12"
+      class="py-2"
+    >
+
+      <v-btn-toggle
+        v-model="field"
+        tile
+        color="deep-purple accent-3"
+        group
+      >
+        <v-btn value="PromedioIngr">
+          Los que ganan más
+        </v-btn>
+
+        <v-btn value="PExperiencia">
+          Con más experiencia
+        </v-btn>
+
+        <v-btn value="PEstudios">
+          Con más estudios superiores
+        </v-btn>
+
+        <v-btn value="PromedioEdad">
+          Los más jovenes
+        </v-btn>
+      </v-btn-toggle>
+    </v-col>
+
+    </div>
+    <div class="partido-ranking">
+      <div class="partido" v-for="partido in graph_data" :key="partido.name">
+        <div class="partido-name">{{ partido.Partido }}</div>
+        <div class="graph">
+          <div class="graph-item" :class="{'active': field == 'PromedioEdad'}">
+            <label>Promedio de edad</label>
+            <div>
+              <span>{{ partido.PromedioEdad}} años</span>
+              <div class="percent promedio-edad" :style="`width: ${calcScale(partido.PromedioEdad, 'PromedioEdad', 85, 150)}px`"></div></div>
+          </div>
+          <div class="graph-item" :class="{'active': field == 'Joven'}">
+            <label>El más joven</label>
+            <div>
+              <span>{{ partido.Joven}} años</span>
+              <div class="percent joven" :style="`width: ${calcScale(partido.Joven, 'Joven', 85, 150)}px`"></div></div>
+          </div>
+          <div class="graph-item" :class="{'active': field == 'Mayor'}">
+            <label>El mayor de todos</label>
+            <div>
+              <span>{{ partido.Mayor}} años</span>
+              <div class="percent mayor" :style="`width: ${calcScale(partido.Mayor, 'Mayor', 85, 150)}px`"></div></div>
+          </div>
+          <div class="graph-item" :class="{'active': field == 'PromedioIngr'}">
+            <label>Ingresos promedio anual</label>
+            <div>
+            <span>S/. {{ partido.PromedioIngr.toFixed(0) }} soles</span>
+            <div class="percent ingresos" :style="`width: ${calcScale(partido.PromedioIngr, 'PromedioIngr', 500000, 150)}px`"></div></div>
+          </div>
+          <div class="graph-item" :class="{'active': field == 'Pmujeres'}">
+            <label>Lista de mujeres </label>
+            <div>
+              <span>{{ partido.Pmujeres}}%</span>
+              <div class="percent mujeres" :style="`width: ${calcScale(partido.Pmujeres, 'Pmujeres', 100, 100)}%`"></div></div>
+          </div>
+          <div class="graph-item" :class="{'active': field == 'PEstudios'}">
+            <label>Estudios superiores</label><div>
+              <span>{{ partido.PEstudios }}%</span>
+              <div class="percent estudios" :style="`width: ${calcScale(partido.PEstudios, 'PEstudios', 100, 100)}%`"></div></div>
+          </div>
+          <div class="graph-item" :class="{'active': field == 'PExperiencia'}">
+            <label>Experiencia</label><div>
+              <span>{{ partido.PExperiencia }}%</span>
+              <div class="percent experiencia" :style="`width: ${calcScale(partido.PExperiencia, 'PExperiencia', 100, 100)}%`"></div></div>
+          </div>
+          <div class="graph-item" :class="{'active': field == 'Vacancia'}">
+            <label>Vacancia</label><div>{{ partido.Vacancia}}</div>
+          </div>
+          <div class="graph-item" :class="{'active': field == 'TipoDemocracia'}">
+            <label>Tipo democracia</label><div>{{ partido.TipoDemocracia}}</div>
+          </div>
+        </div>        
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import LineChart from "../components/Chart";
+import slugify from "slugify";
+import * as d3 from "d3";
+import { orderBy } from "lodash";
 
 export default {
-  name: "resumen",
-  components: {
-    LineChart
+  name: "Partidos",
+  created() {
+    this.$store.dispatch("getPartidos");
+    this.$store.dispatch("getResumen");
   },
   data() {
     return {
-      options: {},
-      datacollection: null,
-      currentTabla: [],
-      tablasResumen: [
-        { id: 2, value: "Sentencias Penales" },
-        { id: 3, value: "Otras Sentencias" },
-        { id: 4, value: "Inclusión de Género" },
-        { id: 1, value: "Experiencia Política" },
-        { id: 5, value: "Ex Congresistas" }
-      ],
-      currentResumen: []
-    };
+      field: 'ingresos'
+    }
   },
   computed: {
-    t1() {
-      return this.$store.state.t1;
-    },
-    t2() {
-      return this.$store.state.t2;
-    },
-    t3() {
-      return this.$store.state.t3;
-    },
-    t4() {
-      return this.$store.state.t4;
+    partidos() {
+      return this.$store.state.partidos;
     },
     resumen() {
       return this.$store.state.resumen;
+    },
+    graph_data() {
+      return orderBy(this.resumen, [this.field], ['desc'])
     }
   },
   methods: {
-    updateTabla() {
-      switch (this.currentResumen.id) {
-        case 1:
-          this.currentTabla = this.t1;
-          this.fillData();
-          this.sendToGA();
-          break;
-        case 2:
-          this.currentTabla = this.t2;
-          this.fillData();
-          this.sendToGA();
-          break;
-        case 3:
-          this.currentTabla = this.t3;
-          this.fillData();
-          this.sendToGA();
-          break;
-        case 4:
-          this.currentTabla = this.t4;
-          this.fillData();
-          this.sendToGA();
-          break;
-        case 5:
-          this.currentTabla = this.$store.state.resumen;
-          this.fillData();
-          this.sendToGA();
-          break;
-      }
+    calcScale(porcentaje, field, max, r) {
+      let myScale = d3.scaleLinear()
+        .domain([0, max])
+        .range([0, r]);
+      return myScale(porcentaje)
     },
-    getHeaders() {
-      switch (this.currentResumen.id) {
-        case 1:
-          return [
-            { text: "# de Candidatos", value: "NumCandidatos" },
-            { text: "Cargos Anteriores", value: "Cargos_Anteriores" }
-          ];
-        case 2:
-          return [
-            { text: "Tipo Delito Penal", value: "strDelitoPenal" },
-            { text: "Cantidad", value: "Delitos_Penales" }
-          ];
-        case 3:
-          return [
-            { text: "Materia Sentencia", value: "strMateriaSentencia" },
-            { text: "Otros Delitos", value: "Otros_delitos" }
-          ];
-        case 4:
-          return [
-            { text: "Region", value: "Region" },
-            { text: "Por Mujeres", value: "PorMujeres1" },
-            { text: "Por Mujeres Final", value: "PorMujeresFinal" }
-          ];
-        case 5:
-          return [
-            { text: "Partido", value: "Partido" },
-            { text: "Ex Congresistas", value: "ExCongresitas" }
-          ];
-      }
-    },
-    fillData() {
-      let etiquetas;
-      let data;
-
-      switch (this.currentResumen.id) {
-        case 1:
-          etiquetas = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.Experiencia_Pol - a.Experiencia_Pol;
-            })
-            .map(item => {
-              return item.Partido;
-            });
-          data = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.Experiencia_Pol - a.Experiencia_Pol;
-            })
-            .map(item => {
-              return item.Experiencia_Pol;
-            });
-          this.datacollection = {
-            labels: etiquetas,
-            datasets: [
-              {
-                label: "# de candidatos que han ejercido otros cargos electos",
-                backgroundColor: "#f87979",
-                data: data
-              }
-            ]
-          };
-          break;
-        case 2:
-          etiquetas = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.Sentencias_Penal - a.Sentencias_Penal;
-            })
-            .map(item => {
-              return item.Partido;
-            });
-          data = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.Sentencias_Penal - a.Sentencias_Penal;
-            })
-            .map(item => {
-              return item.Sentencias_Penal;
-            });
-          this.datacollection = {
-            labels: etiquetas,
-            datasets: [
-              {
-                label: "# de candidatos con sentencias penales declaradas",
-                backgroundColor: "#f87979",
-                data: data
-              }
-            ]
-          };
-          break;
-        case 3:
-          etiquetas = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.Sentencias_otros - a.Sentencias_otros;
-            })
-            .map(item => {
-              return item.Partido;
-            });
-          data = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.Sentencias_otros - a.Sentencias_otros;
-            })
-            .map(item => {
-              return item.Sentencias_otros;
-            });
-          this.datacollection = {
-            labels: etiquetas,
-            datasets: [
-              {
-                label: "# de candidatos con otras sentencias declaradas",
-                backgroundColor: "#f87979",
-                data: data
-              }
-            ]
-          };
-          break;
-        case 4:
-          etiquetas = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.Mujeres - a.Mujeres;
-            })
-            .map(item => {
-              return item.Partido;
-            });
-          data = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.Mujeres - a.Mujeres;
-            })
-            .map(item => {
-              return item.Mujeres;
-            });
-          this.datacollection = {
-            labels: etiquetas,
-            datasets: [
-              {
-                label: "Inclusión de género",
-                backgroundColor: "#f87979",
-                data: data
-              }
-            ]
-          };
-          break;
-        case 5:
-          etiquetas = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.ExCongresitas - a.ExCongresitas;
-            })
-            .map(item => {
-              return item.Partido;
-            });
-          data = this.$store.state.resumen
-            .sort(function(a, b) {
-              return b.ExCongresitas - a.ExCongresitas;
-            })
-            .map(item => {
-              return item.ExCongresitas;
-            });
-          this.datacollection = {
-            labels: etiquetas,
-            datasets: [
-              {
-                label: "Ex Congresistas",
-                backgroundColor: "#f87979",
-                data: data
-              }
-            ]
-          };
-          break;
-      }
-    },
-    // Mandar la pagina visitada a Google Analytics como un custom event
-    sendToGA() {
-      window.gtag("event", "resumen_" + this.currentResumen.value);
+    render_logo(i) {
+      return slugify(i).toLowerCase();
     }
   }
 };
